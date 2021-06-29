6906a378e9c7294e608e62f1fea2feff5c14053e
Simonas Marcišauskas <11994076+simas232@users.noreply.github.com>
2021-06-30 01:04:38 +0200

    feat: add required fields for Todo class

--- ToDoIt/Models/Todo.cs
+++ ToDoIt/Models/Todo.cs
@@ -6,5 +6,9 @@ namespace ToDoIt.Models
 {
     class Todo
     {
+        private readonly int todoId;
+        private String description;
+        private bool done;
+        private Person assignee;
     }
 }
