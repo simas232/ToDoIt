4c7938f7e542f57843da6be5917b6d87013758fb
Shayan Alivand <shayann1500@gmail.com>
2021-07-01 14:42:08 +0200

    Added PersonSequencer class

--- ToDoIt/Data/PersonSequencer.cs
+++ ToDoIt/Data/PersonSequencer.cs
@@ -4,7 +4,22 @@ using System.Text;
 
 namespace ToDoIt.Data
 {
-    class PersonSequencer
+    public class PersonSequencer
     {
+        private static int personId;
+
+        public static int PersonId
+        {
+            get { return personId; }
+            private set { personId = NextPersonId(); }
+        }
+        public static int NextPersonId()
+        {
+            return ++personId;
+        }
+        public static void Reset()
+        {
+            personId = 0;
+        }
     }
 }
