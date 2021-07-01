bedb76a32b7f9e6a663c8685892f3fc276f416da
Simonas Marcišauskas <11994076+simas232@users.noreply.github.com>
2021-07-01 14:27:54 +0200

    feat: add TodoSequencer class

---
+++ ToDoIt.Tests/Data/TodoSequencerTests.cs
@@ -0,0 +1,44 @@
+﻿using Xunit;
+using ToDoIt.Data;
+
+namespace ToDoIt.Tests.Data
+{
+    public class TodoSequencerTests
+    {
+        [Fact]
+        public void TodoSequencer_TestIdIncrement()
+        {
+            // Arrange
+            int firstIdRef = 1;
+            int secondIdRef = 2;
+            int thirdIdRef = 3;
+
+            // Act
+            int firstTodoId = TodoSequencer.NextTodoId();
+            int secondTodoId = TodoSequencer.NextTodoId();
+            int thirdTodoId = TodoSequencer.NextTodoId();
+
+            // Assert        
+            Assert.Equal(firstTodoId, firstIdRef);
+            Assert.Equal(secondTodoId, secondIdRef);
+            Assert.Equal(thirdTodoId, thirdIdRef);
+        }
+
+        [Fact]
+        public void TodoSequencer_TestIdReset()
+        {
+            // Arrange
+            int todoIdRef = 1;
+            int todoId;
+
+            // Act
+            TodoSequencer.NextTodoId();
+            TodoSequencer.NextTodoId();
+            TodoSequencer.Reset();
+            todoId = TodoSequencer.NextTodoId();
+
+            // Assert        
+            Assert.Equal(todoId, todoIdRef);
+        }
+    }
+}
---
+++ ToDoIt/Data/TodoSequencer.cs
@@ -0,0 +1,23 @@
+﻿namespace ToDoIt.Data
+{
+    public class TodoSequencer
+    {
+        private static int todoId;
+
+        public static int TodoId {
+            get { return todoId; }
+            set { todoId = value; }
+        }
+
+        public static int NextTodoId()
+        {
+            TodoId = todoId;
+            return ++TodoId;
+        }
+
+        public static int Reset()
+        {
+            return TodoId = 0;
+        }
+    }
+}
