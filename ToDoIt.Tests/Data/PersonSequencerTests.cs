496f5e9af64afd422c1c93c5081a8c94d6f68dc1
Shayan Alivand <shayann1500@gmail.com>
2021-07-01 14:43:02 +0200

    Added XUnit test for PeopleSequencer class

---
+++ ToDoIt.Tests/Data/PersonSequencerTests.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+using ToDoIt.Models;
+using ToDoIt.Data;
+
+namespace ToDoIt.Tests.Models
+{
+    public class PersonSequencerTests
+    {
+        [Fact]
+        public void PersonSequenceInput()
+        {
+            // Arrange
+            int firstId;
+            int secondId;
+            int firstIdAfterReset;
+            // Act
+            firstId = PersonSequencer.NextPersonId();
+            secondId = PersonSequencer.NextPersonId();
+            PersonSequencer.Reset();
+            firstIdAfterReset = PersonSequencer.NextPersonId();
+
+            // Assert
+            Assert.Equal(1, firstId);
+            Assert.Equal(2, secondId);
+            Assert.Equal(1, firstIdAfterReset);
+
+        }
+    }
+}
