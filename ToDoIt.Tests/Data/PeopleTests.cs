5b41a357d1e8a3276ff07c980fe5187592de49ce
Shayan Alivand <shayann1500@gmail.com>
2021-07-02 17:30:15 +0200

    Added XUnit test for People class

---
+++ ToDoIt.Tests/Data/PeopleTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using ToDoIt.Data;
+using ToDoIt.Models;
+
+namespace ToDoIt.Tests.Data
+{
+    public class PeopleTests
+    {
+        [Fact]
+        public void checkAddPersonTest()
+        {
+            // Arrange
+            //string firstName = "Simons";
+            //string lastName = "Gothenberg";
+
+            // Act
+            //Person testPersons = People.AddPerson(firstName, lastName);
+
+            // Assert        
+            //Assert.Equal(firstName, testPersons.FirstName);
+            //Assert.Equal(lastName, testPersons.LastName);
+            //Assert.Equal(0, testPersons.PersonId);
+
+            Assert.Equal(4, People.Size());
+
+        }
+
+    }
+}
