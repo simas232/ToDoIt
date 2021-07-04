e54a78fd868f2f48e005ead102e839b67e2f7da2
Simonas Marcišauskas <11994076+simas232@users.noreply.github.com>
2021-07-04 22:47:50 +0200

    feat: add references for required references to TodoItemsTests

--- ToDoIt.Tests/Data/TodoItemsTests.cs
+++ ToDoIt.Tests/Data/TodoItemsTests.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Xunit;
+using ToDoIt.Data;
+using ToDoIt.Models;
 
 namespace ToDoIt.Tests.Data
 {
     public class TodoItemsTests
     {
-
+       
     }
 }
