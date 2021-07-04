f269760c2275d38d1d69c8dccdeed65b78f208e6
Simonas Marcišauskas <11994076+simas232@users.noreply.github.com>
2021-07-04 11:06:35 +0200

    doc: add subheaders to TodoItems class

--- ToDoIt/Data/TodoItems.cs
+++ ToDoIt/Data/TodoItems.cs
@@ -7,13 +7,17 @@ namespace ToDoIt.Data
 {
     public class TodoItems
     {
+        // Fields
         private static Todo[] todoArray = new Todo[0];
+
+        // Properties
         public static Todo[] TodoArray
         {
             get { return todoArray; }
             set { todoArray = value; }
         }
 
+        // Methods
         public static int Size()
         {
             return TodoArray.Length;
