7f82a9ed33efd4b5ffa693f3770884337f45ab72
Simonas Marcišauskas <11994076+simas232@users.noreply.github.com>
2021-07-04 22:48:57 +0200

    feat: add unit tests for Size function of TodoItems class

--- ToDoIt.Tests/Data/TodoItemsTests.cs
+++ ToDoIt.Tests/Data/TodoItemsTests.cs
@@ -9,6 +9,49 @@ namespace ToDoIt.Tests.Data
 {
     public class TodoItemsTests
     {
-       
+        [Fact]
+        public void SizeZeroWorks()
+        {
+            // Arrange
+            int expectedSize = 0;
+            int actualSize;
+
+            // Act
+            TodoSequencer.reset();
+            TodoItems actualTodoItems = new TodoItems();
+            actualSize = actualTodoItems.Size();
+
+            // Assert
+            Assert.Equal(expectedSize, actualSize);
+        }
+        [Fact]
+        public void SizeNonZeroWorks()
+        {
+            // Arrange
+            String firstDescription = "Buy coconut milk";
+            String secondDescription = "Go to the gym";
+            String thirdDescription = "Install Visual Studio";
+            int expectedSizeOneTodo = 1;
+            int expectedSizeTwoTodos = 2;
+            int expectedSizeThreeTodos = 3;
+            int actualSizeOneTodo;
+            int actualSizeTwoTodos;
+            int actualSizeThreeTodos;
+
+            // Act
+            TodoItems actualTodoItems = new TodoItems();
+            actualTodoItems.Clear();
+            actualTodoItems.AddTodo(firstDescription);
+            actualSizeOneTodo = actualTodoItems.Size();
+            actualTodoItems.AddTodo(secondDescription);
+            actualSizeTwoTodos = actualTodoItems.Size();
+            actualTodoItems.AddTodo(thirdDescription);
+            actualSizeThreeTodos = actualTodoItems.Size();
+
+            // Assert
+            Assert.Equal(expectedSizeOneTodo, actualSizeOneTodo);
+            Assert.Equal(expectedSizeTwoTodos, actualSizeTwoTodos);
+            Assert.Equal(expectedSizeThreeTodos, actualSizeThreeTodos);
+        }
     }
 }
