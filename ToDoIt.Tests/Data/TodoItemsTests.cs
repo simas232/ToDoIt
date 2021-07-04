34ab790ef763c84d3d707ac32a090944db6d663f
Simonas Marcišauskas <11994076+simas232@users.noreply.github.com>
2021-07-05 01:03:36 +0200

    feat: create separate unit tests for AddTodo and FindAll functions and reorder unit tasks (TodoItems class)

--- ToDoIt.Tests/Data/TodoItemsTests.cs
+++ ToDoIt.Tests/Data/TodoItemsTests.cs
@@ -8,6 +8,44 @@ namespace ToDoIt.Tests.Data
     public class TodoItemsTests
     {
         [Fact]
+        public void AddTodoWorks()
+        {
+            // Arrange
+            String firstDescription = "Buy coconut milk";
+            String secondDescription = "Go to the gym";
+            String thirdDescription = "Install Visual Studio";
+            int expectedFirstTodoId = 1;
+            int expectedSecondTodoId = 2;
+            int expectedThirdTodoId = 3;
+            int expectedSize = 3;
+
+            // Act
+            TodoSequencer.reset();
+            TodoItems actualTodoItems = new TodoItems();
+            actualTodoItems.TodoArray = new Todo[0];// Replicate Clear() function functionality because we do no trust it yet
+            Todo actualFirstTodo = actualTodoItems.AddTodo(firstDescription);
+            Todo actualSecondTodo = actualTodoItems.AddTodo(secondDescription);
+            Todo actualThirdTodo = actualTodoItems.AddTodo(thirdDescription);
+            Todo[] testTodoItemsArray = actualTodoItems.FindAll();
+
+            // Assert
+            Assert.Equal(firstDescription, actualFirstTodo.Description);
+            Assert.Equal(expectedFirstTodoId, actualFirstTodo.TodoId);
+            Assert.Equal(secondDescription, actualSecondTodo.Description);
+            Assert.Equal(expectedSecondTodoId, actualSecondTodo.TodoId);
+            Assert.Equal(thirdDescription, actualThirdTodo.Description);
+            Assert.Equal(expectedThirdTodoId, actualThirdTodo.TodoId);
+
+            Assert.Equal(firstDescription, testTodoItemsArray[0].Description);
+            Assert.Equal(expectedFirstTodoId, testTodoItemsArray[0].TodoId);
+            Assert.Equal(secondDescription, testTodoItemsArray[1].Description);
+            Assert.Equal(expectedSecondTodoId, testTodoItemsArray[1].TodoId);
+            Assert.Equal(thirdDescription, testTodoItemsArray[2].Description);
+            Assert.Equal(expectedThirdTodoId, testTodoItemsArray[2].TodoId);
+            // Since three todo tasks are in actualTodoItems, the length of testTodoItemsArray should also be the same
+            Assert.Equal(expectedSize, testTodoItemsArray.Length);
+        }
+        [Fact]
         public void SizeZeroWorks()
         {
             // Arrange
@@ -17,6 +55,7 @@ namespace ToDoIt.Tests.Data
             // Act
             TodoSequencer.reset();
             TodoItems actualTodoItems = new TodoItems();
+            actualTodoItems.TodoArray = new Todo[0];// Replicate Clear() function functionality because we do no trust it yet
             actualSize = actualTodoItems.Size();
 
             // Assert
@@ -38,7 +77,7 @@ namespace ToDoIt.Tests.Data
 
             // Act
             TodoItems actualTodoItems = new TodoItems();
-            actualTodoItems.Clear();
+            actualTodoItems.TodoArray = new Todo[0];// Replicate Clear() function functionality because we do no trust it yet
             actualTodoItems.AddTodo(firstDescription);
             actualSizeOneTodo = actualTodoItems.Size();
             actualTodoItems.AddTodo(secondDescription);
@@ -52,54 +91,7 @@ namespace ToDoIt.Tests.Data
             Assert.Equal(expectedSizeThreeTodos, actualSizeThreeTodos);
         }
         [Fact]
-        public void AddTodo_FindAllWorks()
-        {
-            // As it is impossible to check AddTodo function independently, functions AddTodo and FindAll are hereby tested at once
-
-            // Arrange
-            String firstDescription = "Buy coconut milk";
-            String secondDescription = "Go to the gym";
-            String thirdDescription = "Install Visual Studio";
-            int expectedFirstTodoId = 1;
-            int expectedSecondTodoId = 2;
-            int expectedThirdTodoId = 3;
-            int expectedSize = 3;
-
-            // Act
-            TodoSequencer.reset();
-            TodoItems actualTodoItems = new TodoItems();
-            actualTodoItems.Clear();
-            Todo actualFirstTodo = actualTodoItems.AddTodo(firstDescription);
-            Todo actualSecondTodo = actualTodoItems.AddTodo(secondDescription);
-            Todo actualThirdTodo = actualTodoItems.AddTodo(thirdDescription);
-            Todo[] testTodoItemsArray = actualTodoItems.FindAll();
-
-            // Assert
-            // Check if recently created todo list items contain correct descriptions and todoIds
-            Assert.Equal(firstDescription, actualFirstTodo.Description);
-            Assert.Equal(expectedFirstTodoId, actualFirstTodo.TodoId);
-            Assert.Equal(secondDescription, actualSecondTodo.Description);
-            Assert.Equal(expectedSecondTodoId, actualSecondTodo.TodoId);
-            Assert.Equal(thirdDescription, actualThirdTodo.Description);
-            Assert.Equal(expectedThirdTodoId, actualThirdTodo.TodoId);
-            // We added three todo tasks, so the size of array should be 3
-            Assert.Equal(expectedSize, actualTodoItems.Size());
-
-            // Repeat the check of assertions again, but this time access todo list through testTodoItemsArray
-            // If everything runs fine here, this suggests that:
-            // i. AddTodo function does not damage non-last entries in Todo array.
-            // ii. FindAll function works fine because it was able to return array with correct Todo objects
-            Assert.Equal(firstDescription, testTodoItemsArray[0].Description);
-            Assert.Equal(expectedFirstTodoId, testTodoItemsArray[0].TodoId);
-            Assert.Equal(secondDescription, testTodoItemsArray[1].Description);
-            Assert.Equal(expectedSecondTodoId, testTodoItemsArray[1].TodoId);
-            Assert.Equal(thirdDescription, testTodoItemsArray[2].Description);
-            Assert.Equal(expectedThirdTodoId, testTodoItemsArray[2].TodoId);
-            // Since three todo tasks are in actualTodoItems, the length of testTodoItemsArray should also be the same
-            Assert.Equal(expectedSize, testTodoItemsArray.Length);
-        }
-        [Fact]
-        public void FindByIdWorks()
+        public void FindAllWorks()
         {
             // Arrange
             String firstDescription = "Buy coconut milk";
@@ -107,20 +99,15 @@ namespace ToDoIt.Tests.Data
             String thirdDescription = "Install Visual Studio";
 
             // Act
-            TodoSequencer.reset();
             TodoItems actualTodoItems = new TodoItems();
             actualTodoItems.Clear();
             actualTodoItems.AddTodo(firstDescription);
             actualTodoItems.AddTodo(secondDescription);
             actualTodoItems.AddTodo(thirdDescription);
-            Todo actualFirstTodo = actualTodoItems.FindById(1);
-            Todo actualSecondTodo = actualTodoItems.FindById(2);
-            Todo actualThirdTodo = actualTodoItems.FindById(3);
+            Todo[] testTodoItemsArray = actualTodoItems.FindAll();
 
             // Assert
-            Assert.Equal(firstDescription, actualFirstTodo.Description);
-            Assert.Equal(secondDescription, actualSecondTodo.Description);
-            Assert.Equal(thirdDescription, actualThirdTodo.Description);
+            Assert.Equal(actualTodoItems.TodoArray, testTodoItemsArray); 
         }
         [Fact]
         public void ClearWorks()
@@ -138,6 +125,30 @@ namespace ToDoIt.Tests.Data
 
             // Assert
             Assert.Equal(expectedSize, actualSize);
+        }
+        [Fact]
+        public void FindByIdWorks()
+        {
+            // Arrange
+            String firstDescription = "Buy coconut milk";
+            String secondDescription = "Go to the gym";
+            String thirdDescription = "Install Visual Studio";
+
+            // Act
+            TodoSequencer.reset();
+            TodoItems actualTodoItems = new TodoItems();
+            actualTodoItems.Clear();
+            Todo expectedFirstTodo = actualTodoItems.AddTodo(firstDescription);
+            Todo expectedSecondTodo = actualTodoItems.AddTodo(secondDescription);
+            Todo expectedThirdTodo = actualTodoItems.AddTodo(thirdDescription);
+            Todo actualFirstTodo = actualTodoItems.FindById(1);
+            Todo actualSecondTodo = actualTodoItems.FindById(2);
+            Todo actualThirdTodo = actualTodoItems.FindById(3);
+
+            // Assert
+            Assert.Equal(expectedFirstTodo, actualFirstTodo);
+            Assert.Equal(expectedSecondTodo, actualSecondTodo);
+            Assert.Equal(expectedThirdTodo, actualThirdTodo);
         }
     }
 }
