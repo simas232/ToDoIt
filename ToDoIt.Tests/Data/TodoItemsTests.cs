55818cd88ad458eae2d625d3b2bd8ac7134b05ab
Simonas Marcišauskas <11994076+simas232@users.noreply.github.com>
2021-07-04 22:49:55 +0200

    feat: add combined unit test for AddTodo and FindAll functions (TodoItems class)

--- ToDoIt.Tests/Data/TodoItemsTests.cs
+++ ToDoIt.Tests/Data/TodoItemsTests.cs
@@ -53,5 +53,52 @@ namespace ToDoIt.Tests.Data
             Assert.Equal(expectedSizeTwoTodos, actualSizeTwoTodos);
             Assert.Equal(expectedSizeThreeTodos, actualSizeThreeTodos);
         }
+        [Fact]
+        public void AddTodo_FindAllWorks()
+        {
+            // As it is impossible to check AddTodo function independently, functions AddTodo and FindAll are hereby tested at once
+
+            // Arrange
+            String firstDescription = "Buy coconut milk";
+            String secondDescription = "Go to the gym";
+            String thirdDescription = "Install Visual Studio";
+            int expectedFirstTodoId = 1;
+            int expectedSecondTodoId = 2;
+            int expectedThirdTodoId = 3;
+            int expectedSize = 3;
+
+            // Act
+            TodoSequencer.reset();
+            TodoItems actualTodoItems = new TodoItems();
+            actualTodoItems.Clear();
+            Todo actualFirstTodo = actualTodoItems.AddTodo(firstDescription);
+            Todo actualSecondTodo = actualTodoItems.AddTodo(secondDescription);
+            Todo actualThirdTodo = actualTodoItems.AddTodo(thirdDescription);
+            Todo[] testTodoItemsArray = actualTodoItems.FindAll();
+
+            // Assert
+            // Check if recently created todo list items contain correct descriptions and todoIds
+            Assert.Equal(firstDescription, actualFirstTodo.Description);
+            Assert.Equal(expectedFirstTodoId, actualFirstTodo.TodoId);
+            Assert.Equal(secondDescription, actualSecondTodo.Description);
+            Assert.Equal(expectedSecondTodoId, actualSecondTodo.TodoId);
+            Assert.Equal(thirdDescription, actualThirdTodo.Description);
+            Assert.Equal(expectedThirdTodoId, actualThirdTodo.TodoId);
+            // We added three todo tasks, so the size of array should be 3
+            Assert.Equal(expectedSize, actualTodoItems.Size());
+
+            // Repeat the check of assertions again, but this time access todo list through testTodoItemsArray
+            // If everything runs fine here, this suggests that:
+            // i. AddTodo function does not damage non-last entries in Todo array.
+            // ii. FindAll function works fine because it was able to return array with correct Todo objects
+            Assert.Equal(firstDescription, testTodoItemsArray[0].Description);
+            Assert.Equal(expectedFirstTodoId, testTodoItemsArray[0].TodoId);
+            Assert.Equal(secondDescription, testTodoItemsArray[1].Description);
+            Assert.Equal(expectedSecondTodoId, testTodoItemsArray[1].TodoId);
+            Assert.Equal(thirdDescription, testTodoItemsArray[2].Description);
+            Assert.Equal(expectedThirdTodoId, testTodoItemsArray[2].TodoId);
+            // Since three todo tasks are in actualTodoItems, the length of testTodoItemsArray should also be the same
+            Assert.Equal(expectedSize, testTodoItemsArray.Length);
+        }
     }
 }
